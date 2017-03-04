1d4ed25605e4e47fb4ea89e5255f66c907a43ecb
Niall Cummins <niall.cummins1@gmail.com>
2017-03-03 17:01:13 -0700

    Finished Tracker Component

---
+++ app/arcade/arcade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    styleUrls:['app/arcade/arcade.component.css'],
+    templateUrl: 'app/arcade/arcade.component.html'
+})
+
+export class ArcadeComponent{
+    
+}
--- db/product/get_history.sql
+++ db/product/get_history.sql
@@ -1,4 +1,4 @@
 SELECT * FROM orders
 JOIN products_in_order on orders.order_id = products_in_order.order_id
-JOIN products on products_in_order.order_id=products.product_id
+JOIN products on products_in_order.product_id=products.product_id
 WHERE user_id = $1 and completed_date IS NOT NULL;
